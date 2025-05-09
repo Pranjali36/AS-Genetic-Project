8399a0514fc76ac5f689fcc469fb27ad6f16e6eb
Pranjali36 <pranjali.thakur@etu.unige.ch>
2025-05-09 19:35:18 +0200

    Update app4.py

--- app4.py
+++ app4.py
@@ -90,7 +90,7 @@ st.markdown("---")
 
 # ==== Manual Tampering ====
 #st.markdown("<br>", unsafe_allow_html=True)  # Adds vertical space
-st.markdown("<h4 style='color:#2F4F4F;'>🛠️ Tamper Block Metadata</h4>", unsafe_allow_html=True)
+st.markdown("<h4 style='color:#2F4F4F;'>🛠️ Tamper Metadata</h4>", unsafe_allow_html=True)
 col1, col2 = st.columns(2)
 with col1:
     server_to_edit = st.selectbox("Select Server:", ["Server 1", "Server 2", "Server 3"])
